7fe3862fb7403fa39b482ee8589e2d85b9c72771
mannicar <cmannino@mit.edu>
2013-04-17 18:40:11 -0400

    Added autocomplete javascript, modified profile to have autocomplete

---
+++ autocomplete.js
@@ -0,0 +1,8 @@
+$(function() {
+
+    var books = {'Artemis Fowl', 'Holes', 'Eragon', 'Moby Dick', 'Twilight'};
+    var availableTags = books;
+    $("#profileRecommendBook").autocomplete({source: availableTags});
+    
+});
+    
