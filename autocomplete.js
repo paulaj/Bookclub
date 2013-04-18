40d832662445e7b02ebdcb8c27ba2adf9b7a78be
mannicar <cmannino@mit.edu>
2013-04-17 21:55:02 -0400

    Implemented autocomplete for profile.

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    var books = {'Artemis Fowl', 'Holes', 'Eragon', 'Moby Dick', 'Twilight'};
+    var books = ['Artemis Fowl', 'Holes', 'Eragon', 'Moby Dick', 'Twilight'];
     var availableTags = books;
     $("#profileRecommendBook").autocomplete({source: availableTags});
     
