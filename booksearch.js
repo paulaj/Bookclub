f9a1befc034eadc0ed2a6876aa3901c326edd5ec
Paula <paulaj@mit>
2013-05-06 15:14:57 -0400

    commit

---
+++ booksearch.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	Parse.initialize("qyuc8DGipEXPi3Fh32EKqnH2H563DPoFqcRjoa9h", "QTmatMd6trXNFaB0OaaPWEeCdCFpWm6YLv53dnn9");
+	var Book = Parse.Object.extend("Book");
+	var query = new Parse.Query(Book);
+	query.find({
+		success: function(results) {
+			for (var i = 0; i<results.length; i++){
+				console.log(results[i].get("url"));
+				$(".readinglist").append("<a href='"+results[i].get('url')+".html' class='listedBook'>"+results[i].get('title')+" by "+results[i].get('author')+"</a><br/><br/>");
+			}
+		},
+	});
+});
