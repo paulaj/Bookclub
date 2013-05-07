0bb0008cfbb688e725d386b483f4f1d3e2091f25
Elizabeth Attaway <attawaye@mit.edu>
2013-05-07 17:31:09 -0400

    Uh more stuff to commit I guess?
    Why didn't it show these files in the first place gosh darn it

--- profile.js
+++ profile.js
@@ -15,6 +15,29 @@ $.extend({
 		return $.getUrlVars()[name];
 	}
 });
+
+function sendRecommendation(){
+	var bookTitle = $("#profileRecommendBook").val();
+	console.log(bookTitle);
+	Parse.initialize("qyuc8DGipEXPi3Fh32EKqnH2H563DPoFqcRjoa9h", "QTmatMd6trXNFaB0OaaPWEeCdCFpWm6YLv53dnn9");
+	var query = new Parse.Query(Parse.User);
+	var Recommendation = Parse.Object.extend("Recommendation");
+	var username = $.getUrlVar('username');
+	username = username.replace("%20"," ");
+	console.log(username);
+	query.equalTo('username', username);
+	query.first({
+		success: function(person){
+			var rec = new Recommendation();
+			console.log(Parse.User.current().get("username"));
+			rec.set("recommendedBy", Parse.User.current());
+			rec.set("title", bookTitle);
+			rec.set("recommendedTo", person)
+			rec.save();
+		},
+	});
+	}
+
 $(function(){
   Parse.initialize("qyuc8DGipEXPi3Fh32EKqnH2H563DPoFqcRjoa9h", "QTmatMd6trXNFaB0OaaPWEeCdCFpWm6YLv53dnn9");
 		var query = new Parse.Query(Parse.User);
@@ -59,7 +82,26 @@ $(function(){
 					}
 					if (Parse.User.current() != null){
 						var user = Parse.User.current();
+						if (user.get("friends").indexOf(person.get("username")) != -1){
+							console.log("yeah dude");
+							$("#recommendButton").append("<div><h5>Recommend a book: <input class='text' id='profileRecommendBook'><input type='submit' onclick='sendRecommendation()' value='Send'></input></input></h5></div><br>");
+						}
+						$(function() {
+							var bookQuery = new Parse.Query(Book);
+							var bookTitles = []
+							bookQuery.find({
+								success: function(books){
+									for (var i = 0; i<books.length; i++){
+										bookTitles.push(books[i].get("title"));
+									}
+									console.log(bookTitles);
+									$("#profileRecommendBook").autocomplete({source: bookTitles});  
+								},
+							});
+ 
+						});
 					}
+
 				} else {
 					alert("That user doesn't exist!")
 					window.location = "friendsList.html";
